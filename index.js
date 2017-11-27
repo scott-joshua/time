003eec7802656b830210cfd1f18d7c43496748a7
Joshua <scott.joshua@gmail.com>
2017-11-27 16:58:38 -0700

    added options call

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict';
+
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
     currentTime.setTimezone("America/Denver");
     callback(null, {
         statusCode: '200',
+        headers: {"Access-Control-Allow-Origin": "*"},
         body:  JSON.stringify({ time:currentTime.getTime()}),
+    });
+};
+
+exports.options = (event, context, callback) => {
+    callback(null, {
+        statusCode: '200',
+        headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "OPTIONS, GET"}
     });
 };
